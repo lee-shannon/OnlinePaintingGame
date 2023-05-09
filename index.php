5564469feb617ad8e1eb2b104990b763f830d209
lee-shannon <96852559+lee-shannon@users.noreply.github.com>
2023-05-09 14:43:13 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<style>
+    h2 {
+        color: #2E5984;
+    }
+
+    h4 {
+        color: #1E3F66;
+    }
+</style>
+
+
+
+<h2> Thank you for visiting our website </h2>
+
+<div id = "quote">
+<h4> Spectrum has conducted successful color therapy since <strong>2001</strong>
+<br>You can trust us with your journey by starting <a href="color">here</a>.</p>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>
