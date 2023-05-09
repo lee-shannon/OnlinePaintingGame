a22e21ba80d3121a7bc1e0a89320925fd7f0877a
lee-shannon <96852559+lee-shannon@users.noreply.github.com>
2023-05-09 14:38:44 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,39 @@
+<h3> Meet the Team </h3>
+
+<div id="shan">
+<img src="https://media-exp1.licdn.com/dms/image/C4D03AQH05T8TJKBdQQ/profile-displayphoto-shrink_800_800/0/1647385308250?e=1653523200&v=beta&t=JU357GadwjYXcLBDSJkO0JLugLL6EQN5SUFLTZ9qjbk" alt="Shannon Lee"
+     width="400"
+     height="300">
+<h2>Shannon Lee</h2>
+<p>
+	Hi there! My name is Shannon and I'm currently a Senior studying computer science and business administration. I'm currently undergoing a product management internship and hope to work in either project management or software development in the future!
+	I love spending my down time reading, or with friends. A fun fact about me is that I have a shiba inu named Loki and a grey tabby named Cutie, both of whom I love very much!
+</p>
+</div>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+<div id="linds">
+<img src="https://media-exp1.licdn.com/dms/image/C4E03AQG0jW2F1wXSxw/profile-displayphoto-shrink_400_400/0/1647899064249?e=1654128000&v=beta&t=JEhuwUKtLxMLg7SgVDi3D5IWgPTMGqIu6E7RgR2ztyQ" alt="Lindsey Larson">
+    <h2>Lindsey Larson</h2>
+    <p>Hi! I'm Lindsey and am currently a 3rd year Applied Computing Technology student at Colorado State University. I am employed by CSU in the Library Technical Services where I assist in maintaining databases, various applications and websites used in the Library. Currently, I am also a Student Intern for an organization called Young Life, where I get to mentor and lead middle school girls.
+        I enjoy being outside specifically in the mountains on a warm summer day, crafting, sewing, and being with friends!</p>
+</div>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+<div id="freya">
+<img src="https://media-exp1.licdn.com/dms/image/C4E03AQE5YcsDRNf_Dw/profile-displayphoto-shrink_400_400/0/1646260789474?e=1654128000&v=beta&t=x-Ca267mOoZjj9gFxHKN0JLNSrHFSror6nleVOXfoJk" alt="Freya LaLuna">
+<h2>Freya LaLuna</h2>
+<p> Hey! I'm Freya LaLuna, and I'm currently a junior majoring in Computer Science with a minor in Ethnic Studies. I'm currently employed as a peer educator for the SEED office on campus, where I facilitate workshops on social identity, bias, and communication. 
+I love reading, creative writing, baking, and long-distance running! I also have two kittens, Tom and Sasha - the former is a rescue, and the latter adopted me late last year. </p>
+</div>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<div id="arysa">
+<img src="https://media-exp1.licdn.com/dms/image/C5603AQH2z4CU6O2xuw/profile-displayphoto-shrink_400_400/0/1591061791675?e=1654128000&v=beta&t=18tQR0gEJpThjKFW9Iv6ZJLZDEo0AgpRQ5o_lbasXD0" alt="Arysa Flores">
+<h2>Arysa Flores</h2>
+<p>Hello! My name is Arysa Flores and I am currently a senior majoring in Computer Science with a concentration in 
+    Human-Centered Computing. I love to read, write, and listen to music while hanging out with my pet cat Pluto. She is four years old and is a little bit 
+    grumpy for her age, but once she trusts you she is very sweet (most of the time)!
+</p>
+</div>
