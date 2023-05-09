a8128108ff7775691fc373c3d21272199caf2eed
lee-shannon <96852559+lee-shannon@users.noreply.github.com>
2023-05-09 14:41:39 -0600

    Create db.php

---
+++ db.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.2
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2019 Fuel Development Team
+ * @link       https://fuelphp.com
+ */
+
+/**
+ * -----------------------------------------------------------------------------
+ *  Global database settings
+ * -----------------------------------------------------------------------------
+ *
+ *  Set database configurations here to override environment specific
+ *  configurations
+ *
+ */
+
+return array(
+                        'default' => array(
+                            'connection'  => array(
+                                'dsn'        => 'mysql:host=faure.cs.colostate.edu;dbname=lnlarson',
+                                'username'   => 'lnlarson',
+                                'password'   => '832325131',
+                            ),
+                        ),
+
+);
